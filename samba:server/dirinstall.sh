c88b1234af15cbdc8e2c4441fdb7de4f71f5273a
isx53320159 <isx53320159@i12.informatica.escoladeltreball.org>
2019-01-11 09:53:38 +0100

    prova

---
+++ samba:server/dirinstall.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# @edt ASIX M06 2018-2019
+# instalacio de carpetas
+# -------------------------------------
+mkdir -p /tmp/home/pere
+mkdir -p /tmp/home/pau
+mkdir -p /tmp/home/anna
+
+chown -R 5001.100 /tmp/home/pere
+chown -R 5000.100 /tmp/home/pau
+chown -R 5002.100 /tmp/home/anna
