8e9c90fae2c89267d5ee86ed71366f9a89899389
raulbaena <raulbaenanocea@gmail.com>
2019-02-21 12:26:44 +0100

    Update startup.sh

--- samba:server/startup.sh
+++ samba:server/startup.sh
@@ -8,7 +8,7 @@
 /usr/sbin/nscd && echo "nscd Ok"
 /usr/sbin/smbd && echo "snmb Ok"
 /usr/sbin/nmbd && echo "nmb Ok"
-#/opt/docker/usuaris_samba.sh && echo "Usuaris instalats"
-#/opt/docker/dir_samba.sh  && echo "Directoris samba creats"
+/opt/docker/usuaris_samba.sh && echo "Usuaris instalats"
+/opt/docker/dir_samba.sh  && echo "Directoris samba creats"
 /bin/bash
 
