5b52c7a8a49bd2bbe064a820539cb29c3a40a5e7
root <root@localhost.localdomain>
2019-02-21 13:18:29 +0100

    Final v3

--- samba:server/dir_samba.sh
+++ samba:server/dir_samba.sh
@@ -13,13 +13,10 @@ mkdir -p /tmp/home/2asix/vladimir
 cp /opt/docker/README.md /tmp/home/pere/README.md
 cp /opt/docker/README.md /tmp/home/marta/README.md
 cp /opt/docker/README.md /tmp/home/pau/README.md
-cp /opt/docker/README.md /tmp/home/2asix/vladimir/README.md
-
-mkdir -p /tmp/home/pere
-mkdir -p /tmp/home/pau
-mkdir -p /tmp/home/marta
-mkdir -p /tmp/home/anna
+cp /opt/docker/README.md /tmp/home/2asix/vladimir/README.me
 
 chown -R 5001.100 /tmp/home/pere
 chown -R 5000.100 /tmp/home/pau
 chown -R 5003.100 /tmp/home/marta
+chown -R 5003.100 /tmp/home/2asix/vladimir
+
