5486179c03673f25f8f14009496e0ccea0305734
raulbaena <raulbaenanocea@gmail.com>
2019-02-21 13:10:10 +0100

    Update install.sh

--- samba:server/install.sh
+++ samba:server/install.sh
@@ -9,4 +9,5 @@ cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
 cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
 cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
 cp /opt/docker/system-auth /etc/pam.d/system-auth
+cp /opt/docker/smb.conf /etc/samba/smb.conf
 
