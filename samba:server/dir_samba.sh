c43129b92c016a87e97967d0bc4537d62e71a3c3
raulbaena <raulbaenanocea@gmail.com>
2019-02-21 13:12:34 +0100

    Update dir_samba.sh

--- samba:server/dir_samba.sh
+++ samba:server/dir_samba.sh
@@ -15,8 +15,11 @@ cp /opt/docker/README.md /tmp/home/marta/README.md
 cp /opt/docker/README.md /tmp/home/pau/README.md
 cp /opt/docker/README.md /tmp/home/2asix/vladimir/README.md
 
-chown -R pere.hisx2 /tmp/home/pere
-chown -R pau.hisx2 /tmp/home/pau
-chown -R marta.hisx2 /tmp/home/marta
-chown -R 11011.10001 /tmp/home/2asix/user10
-chown -R vladimir.10001 /tmp/home/2asix/vladimir
+mkdir -p /tmp/home/pere
+mkdir -p /tmp/home/pau
+mkdir -p /tmp/home/marta
+mkdir -p /tmp/home/anna
+
+chown -R 5001.100 /tmp/home/pere
+chown -R 5000.100 /tmp/home/pau
+chown -R 5003.100 /tmp/home/marta
